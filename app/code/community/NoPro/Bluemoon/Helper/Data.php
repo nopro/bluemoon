1b763eb0e9a5ee200540fe90b1891772244e0189
nopro <nopro4biz@gmail.com>
2017-04-21 16:09:45 +0000

    modified:   app/code/community/NoPro/Bluemoon/Helper/Data.php 	modified:   app/code/community/NoPro/Bluemoon/Model/Bluemoon.php 	modified:   app/code/community/NoPro/Bluemoon/Model/Observer.php 	modified:   app/code/community/NoPro/Bluemoon/etc/config.xml 	modified:   app/code/community/NoPro/Bluemoon/etc/system.xml 	modified:   app/design/frontend/base/default/layout/nopro_bluemoon.xml 	modified:   package.xml

--- app/code/community/NoPro/Bluemoon/Helper/Data.php
+++ app/code/community/NoPro/Bluemoon/Helper/Data.php
@@ -2,8 +2,15 @@
 
 class NoPro_Bluemoon_Helper_Data extends Mage_Core_Helper_Data {
 
-  public function isEnabled() {
-    return 1;
-  }
+    public function isEnabled() {
+        return Mage::getStoreConfig('nopro_bluemoon/view/enabled');
+    }
   
+    public function log($msg) {
+        Mage::log($msg, null, 'bluemoon.log');
+    }
+  
+    public function getPartnerId() {
+        return Mage::getStoreConfig('nopro_bluemoon/view/partner_id');
+    }
 }
--- app/code/community/NoPro/Bluemoon/Model/Observer.php
+++ app/code/community/NoPro/Bluemoon/Model/Observer.php
@@ -2,4 +2,11 @@
 
 class NoPro_Bluemoon_Model_Observer
 {
+    protected $helper;
+
+    public function __construct()
+    {
+        $this->helper = Mage::helper('nopro_bluemoon');
+    }
+
 }
